5adea8857d903c519c3e19248ca75037f72a286b
Vetik007 <115701840+Vetik007@users.noreply.github.com>
2023-10-11 19:02:33 +0000

    Deploying to gh-pages from @ Vetik007/Car@32bd426566025442b4873a49629a78812002a421 🚀

--- assets/FavoritesPage-5ceb7a95.js
+++ assets/FavoritesPage-5ceb7a95.js
@@ -1,4 +1,4 @@
-import{n,L as w,r as o,u as v,g as b,j as t}from"./index-c4be7e8d.js";import{u as S,d as k,S as F,a as I,M as C,n as L}from"./defolt-6114de43.js";const M=n.div`
+import{n,L as w,r as o,u as v,g as b,j as t}from"./index-9d8928a4.js";import{u as S,d as k,S as F,a as I,M as C,n as L}from"./defolt-e8d2f77c.js";const M=n.div`
   /* position: relative; */
   /* overflow-x: hidden; */
   height: 100vh;
