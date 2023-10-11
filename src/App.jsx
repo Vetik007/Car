fc0f9e67556dda17835bff0bf1e541aa29c11146
Vetik007 <boy0007@ukr.net>
2023-10-11 18:29:22 +0300

    update App.js - Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
-import HomePage from './pages/HomePage/HomePage';
-import CatalogPage from './pages/CatalogPage/CatalogPage';
-import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
-import ErrorPage from './pages/ErrorPage/ErrorPage';
+
+import { lazy } from 'react';
+
 import { AppWrapper } from './App.styled';
+
+// import HomePage from './pages/HomePage/HomePage';
+// import CatalogPage from './pages/CatalogPage/CatalogPage';
+// import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage/FavoritesPage'));
 
 function App() {
   return (
@@ -15,7 +22,7 @@ function App() {
           <Route path="/catalog" element={<CatalogPage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
 
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="*" element={<HomePage />} />
         </Route>
       </Routes>
     </AppWrapper>
